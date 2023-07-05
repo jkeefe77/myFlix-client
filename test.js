88f9a6b5a9c56e10a3fa684b24596c387cf57c2d
jkeefe77 <jvkeefe@gmail.com>
2023-07-05 19:07:53 -0400

    package.json
    npm init package.json

---
+++ test.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser"),
+  uuid = require("uuid");
+const port = 8080;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log("Your app is listening on port ${port}");
+});
