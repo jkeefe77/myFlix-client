c8675854de6dfd4538af499b306f5123c2ab7635
jkeefe77 <jvkeefe@gmail.com>
2023-07-10 14:58:09 -0400

    login / signup additions
    login-view / signup-view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,3 @@
-// export const MovieCard = ({ movie, onMovieClick }) => {
-//   return (
-//     <div
-//       onClick={() => {
-//         onMovieClick(movie);
-//       }}
-//     >
-//       {movie.Title}
-//     </div>
-//   );
-// };
 import React from "react";
 import PropTypes from "prop-types";
 
