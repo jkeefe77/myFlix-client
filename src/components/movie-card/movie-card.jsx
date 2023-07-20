1c85e4bc37e462a7bcfc27356b214393a1bd1b3c
jkeefe77 <jvkeefe@gmail.com>
2023-07-20 14:58:00 -0400

    movie.card.jsx
    changed to movie._id

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -6,12 +6,11 @@ import { Link } from "react-router-dom";
 export const MovieCard = ({ movie }) => {
   console.log("movie ", movie);
   return (
-    <Link className="text-decoration-none" to={"/movies/$(movieId)}"}>
-      <h1>{movie.Title}</h1>
+    <Link className="text-decoration-none" to={`/movies/${movie._id}`}>
       <Card className="h-100" border="light">
         <Card.Img variant="top" src={movie.imagePath} className="border" />
         <Card.Body className="text-center">
-          <Card.Title className="fw-bold">{movie.title}</Card.Title>
+          <Card.Title className="fw-bold">{movie.Title}</Card.Title>
           <Card.Text className="fs-6">{movie.Genres}</Card.Text>
         </Card.Body>
       </Card>
@@ -21,6 +20,7 @@ export const MovieCard = ({ movie }) => {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
     _id: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
