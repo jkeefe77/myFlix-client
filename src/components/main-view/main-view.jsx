7e4b318234f63822099aa7400ec3bedf3e64726e
jkeefe77 <jvkeefe@gmail.com>
2023-07-29 19:47:26 -0400

    main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -124,14 +124,7 @@ export const MainView = () => {
                 ) : (
                   <>
                     {movies.map((movie) => (
-                      <Col
-                        key={movie._id}
-                        xxl={4}
-                        xl={6}
-                        lg={12}
-                        md={12}
-                        xs={12}
-                      >
+                      <Col key={movie._id} xxl={4} xl={6} lg={12} md={12} xs={12}>
                         <MovieCard movie={movie} />
                       </Col>
                     ))}
--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -2,26 +2,29 @@ import React from "react";
 
 export const UserInfo = ({ user }) => {
   return (
+    <div className="contact-form-container-fluid" style={{backgroundColor:"#B4BFA3" }}>
     <div className="user-info-container">
       <div className="user-info-details">
-        <h2 className="mydetails">Account Information</h2>
-        <table className="userdetails">
-          <tbody>
-            <tr>
-              <td className="fw-bold">Username:</td>
-              <td>{user.Username}</td>
-            </tr>
-            <tr>
-              <td className="fw-bold">Email:</td>
-              <td>{user.Email}</td>
-            </tr>
-            <tr>
-              <td className="fw-bold">Birthdate:</td>
-              <td>{user.Birthday.slice(0, 10)}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+       <h2 className="mydetails">Account Information</h2>
+      <table className="userdetails">
+        <tbody>
+          <tr>
+            <td className="fw-bold">Username:</td>
+            <td>{user.Username}</td>
+          </tr>
+          <tr>
+            <td className="fw-bold">Email:</td>
+            <td>{user.Email}</td>
+          </tr>
+          <tr>
+            <td className="fw-bold">Birthdate:</td>
+            <td>{user.Birthday.slice(0, 10)}</td>
+          </tr>
+        </tbody>
+      </table>
+     </div>
+    </div>
     </div>
   );
 };
+
